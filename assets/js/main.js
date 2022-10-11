e631a28bbeeb744a515eca5e141ef1a6f80bc4ea
manuelgaleone <galeonemanuel14@gmail.com>
2022-10-11 13:32:30 +0200

    Add Mail function

---
+++ assets/js/main.js
@@ -0,0 +1,29 @@
+/*
+
+    Mail
+    Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo.
+
+    Gioco dei dadi
+    Generare un numero random da 1 a 6, sia per il giocatore sia per il computer. Stabilire il vincitore, in base a chi fa il punteggio più alto.
+
+*/
+
+//Chiedo la propria Mail
+let userMail = prompt("Qual è la tua E-Mail?");
+
+//Inserisco la Mail nella Lista d'accesso
+let mailList = ["mario.rossi@gmail.com", "antonio.luigi@libero.it", userMail];
+
+//Analizzo la Mail List
+for (i = 0; i < mailList.length; i++) {
+
+    //Controllo se la Mail dell'Utente c'è o meno
+    if (mailList.indexOf(userMail) !== -1) {
+        //Se c'è, gli dico che c'è
+        console.log("La tua Mail c'è, puoi entrare.")
+    } else {
+        //Se non la trovo, gli comunico che non c'è
+        console.log("La tua Mail non c'è.")
+    }
+
+}
